70ffacb433988769ea9e4cd0ddabfd6cfd269aa9
Jcasanova1990 <jcasanova1990@gmail.com>
2025-06-26 09:43:31 -0400

    first

---
+++ stringBasics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string my_string = "Ayogfuy";
+    int len_string = my_string.length();
+
+    cout << len_string << endl;
+
+    return 0;
+}
